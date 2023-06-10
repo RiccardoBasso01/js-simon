cbd5de7528a8a18d24d1b68999a4c53ea1c3fedd
RiccardoBasso01 <riccardo.basso2001@gmail.com>
2023-06-10 16:53:55 +0200

    Aggiunta 5 numeri random

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
 console.log('JS OK');
 
-// 1 PUNTO: timer di 30 secondi
+const game = document.getElementById('game');
+const preGame = document.getElementById('pre-game');
+
+const play = document.getElementById('play');
+const timer = document.getElementById('timer');
+
+const number = document.querySelectorAll('.number');
+
+// Al click "INIZIA" 
+play.addEventListener('click', function () {
+    preGame.classList.add('d-none');
+    game.classList.remove('d-none');
+
+    // 5 numeri da indovinare
+    for(let i = 0; i < number.length; i++){
+        number[i].innerText = randomNumber(); 
+    }
+    
+    // Timer di 30s
+    let second = 30;
+    timer.innerText = second;
+
+    const time = setInterval(() => {
+        timer.innerText = --second;
+        if (second <= 10 && second > 0) {
+            timer.classList.add('last-seconds');
+        } else if (second <= 0) {
+            clearInterval(time)
+        }
+    }, 1000);
+})
+
+// Funzione numeri random da 0 a 99
+function randomNumber(){
+    const random = Math.floor(Math.random() * 99);
+    return random;    
+}
