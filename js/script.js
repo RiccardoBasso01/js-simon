ebb20f016fca4e5ed230d2984ed68ba14c9261e8
RiccardoBasso01 <riccardo.basso2001@gmail.com>
2023-06-11 21:11:41 +0200

    Aggiunta coriandoli

--- js/script.js
+++ js/script.js
@@ -74,7 +74,9 @@ play.addEventListener('click', function () {
                 resultValue += `<div class="wrong">${[i + 1 + '°']} Sbagliato </div>`
             }
         }
-        console.log(resultValue)
+        // Controllo punteggio massimo
+        if(point === 5) firework();
+
         // Risultato
         result.classList.remove('d-none');
         result.innerHTML = resultValue + `<div class="result">Hai totalizzato ${point} punti </div><button id="restart">Ricomincia</button>`;
@@ -93,4 +95,30 @@ play.addEventListener('click', function () {
 function randomNumber() {
     const random = Math.floor(Math.random() * 99);
     return random;
+}
+
+
+// Fuochi d'artificio se si fa il punteggio massimo
+function firework() {
+
+    let duration = 15 * 1000;
+    let animationEnd = Date.now() + duration;
+    let defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    let interval = setInterval(function () {
+        let timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+            return clearInterval(interval);
+        }
+
+        let particleCount = 50 * (timeLeft / duration);
+        // since particles fall down, start a bit higher than random
+        confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+        confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+    }, 250);
 }
